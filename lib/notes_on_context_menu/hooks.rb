f0f75341bd386a9791937d77a6d939bd32959a01
sk-ys <yskrmt@gmail.com>
2023-10-27 12:34:58 +0900

    Rename folder

--- lib/notes_on_context_menu/hooks.rb
+++ lib/notes_on_context_menu/hooks.rb
@@ -1,4 +1,4 @@
 class NotesOnContextMenu::Hooks < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_html_head, :partial => "notes_on_context_menu/layouts_base_html_head"
-  render_on :view_issues_context_menu_end, :partial => "notes_on_context_menu/issues_context_menu_end"
+  render_on :view_layouts_base_html_head, :partial => "notes_on_context_menus/layouts_base_html_head"
+  render_on :view_issues_context_menu_end, :partial => "notes_on_context_menus/issues_context_menu_end"
 end
