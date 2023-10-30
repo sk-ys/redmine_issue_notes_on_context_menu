690a304a3f702358ec12f39de19f86e90620e7d3
sk-ys <yskrmt@gmail.com>
2023-10-30 23:51:59 +0900

    v0.1.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_issue_notes_on_context_menu do
   name 'Redmine Issue Notes On Context Menu plugin'
   author 'sk-ys'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/sk-ys/redmine_issue_notes_on_context_menu'
   author_url 'https://github.com/sk-ys'
 end
